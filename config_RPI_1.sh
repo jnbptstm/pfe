2c9b329541061fb226fed1e35af1a601b85e602c
root <root@raspberrypi.(none)>
2014-01-13 15:59:31 +0000

    Few changes...

--- config_RPI_1.sh
+++ config_RPI_1.sh
@@ -15,13 +15,15 @@ iwconfig wlan0 channel 1 essid Shared mode ad-hoc
 ifconfig wlan0 up
 ifconfig wlan0 192.168.1.2 netmask 255.255.255.0
 
-# Adding HWaddr of the router to permit ping:
+# Adding HWaddr of the router to permit ping
 arp -s 192.168.1.1 00:26:5E:30:C1:C1
 
-# Adding HWaddr of the 2nd RPI to permit ping:
+# Adding HWaddr of the 2nd RPI to permit ping
 arp -s 192.168.1.3 80:1F:02:A6:A6:09
 
-# Adding default route:
+# Adding default route
 route add default gw 192.168.1.1 wlan0
+
+
 
 echo End of configuration.
