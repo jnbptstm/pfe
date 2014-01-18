ea012859b4a1ce48b3b31bc50b0b30685eabebb5
root <root@raspberrypi.(none)>
2014-01-18 17:12:53 +0000

    new stuff

--- python_scripts/capturingToNetworkStream_Sender.py
+++ python_scripts/capturingToNetworkStream_Sender.py
@@ -7,7 +7,8 @@ import picamera
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 client_socket = socket.socket()
-client_socket.connect(('my_server', 8000))
+# client_socket.connect(('my_server', 8000))
+client_socket.connect(("localhost", 8010))
 
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
--- python_scripts/recordingToANetworkStream.py
+++ python_scripts/recordingToANetworkStream.py
@@ -5,7 +5,9 @@ import picamera
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 client_socket = socket.socket()
-client_socket.connect(('my_server', 8000))
+host = socket.gethostname()
+port = 1234
+client_socket.connect((host, port))
 
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
