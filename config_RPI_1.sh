840e81e5b079053d910a250cdfbe65a8819b0404
Jean-Baptiste MARTIN <jnbptstm@gmail.com>
2014-01-29 02:09:16 +0100

    Update config_RPI_1.sh

--- config_RPI_1.sh
+++ config_RPI_1.sh
@@ -5,7 +5,6 @@ echo Starting configuration...
 ifconfig eth0 down
 ifconfig eth0 192.168.2.2 netmask 255.255.255.0
 
-echo arp -s 192.168.1.1 00:26:5E:30:C1:C1
 arp -s 192.168.2.1 B8:27:EB:2F:06:0F
 
 route add default gw 192.168.2.2 eth0
