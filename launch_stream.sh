e7122611fe22f438b30bfc17a7d9caa192c0bc39
Jean-Baptiste MARTIN <jnbptstm@gmail.com>
2014-01-29 01:44:45 +0100

    Delete launch_stream.sh

--- launch_stream.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-raspivid -t 0 -w 480 -h 260 -fps 25 -b 800000 -p 0,0,640,480 -o - | gst-launch -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=192.168.1.2 port=5000
