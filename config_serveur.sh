455008021980a97e2712b522c24c57b5af6f018b
fabienbcool <fabien.bascoul@gmail.com>
2014-01-30 11:18:18 +0100

    Update and rename config_RPI_2.sh to config_serveur.sh

--- config_serveur.sh
+++ config_serveur.sh
@@ -15,14 +15,15 @@ ifconfig wlan0 up
 ifconfig wlan0 192.168.1.3 netmask 255.255.255.0
 
 # Adding HWaddr of the router to permit ping:
-arp -s 192.168.1.3 00:26:5E:30:C1:C1
+arp -s 192.168.2.1 00:26:5E:30:C1:C1
 
 # Adding HWaddr of the 2nd RPI to permit ping:
-arp -s 192.168.2.1 B8:27:EB:7E:15:22
+arp -s 192.168.1.1 B8:27:EB:7E:15:22
 
 # Adding default route:
 route add default gw 192.168.1.1 wlan0
 
+ifconfig eth0 192.168.2.3 netmask 255.255.255.0
 # Nameserver has been added in /etc/resolv.conf (10.188.0.1)
 
 echo End of configuration.
