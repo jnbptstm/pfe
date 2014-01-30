6758df66f4b604e3e9c630eac4ae381bf6a8e9df
fabienbcool <fabien.bascoul@gmail.com>
2014-01-30 11:23:20 +0100

    Update config_serveur.sh

--- config_serveur.sh
+++ config_serveur.sh
@@ -15,7 +15,7 @@ ifconfig wlan0 up
 ifconfig wlan0 192.168.1.3 netmask 255.255.255.0
 
 # Adding HWaddr of the router to permit ping:
-arp -s 192.168.2.1 00:26:5E:30:C1:C1
+arp -s 192.168.2.10 78:45:C4:B8:9E:18
 
 # Adding HWaddr of the 2nd RPI to permit ping:
 arp -s 192.168.1.1 B8:27:EB:7E:15:22
